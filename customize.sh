2c412bf68340000344d4a3b52163b3a7225df9af
Jakub668 <jakub.hunek19@gmail.com>
2026-03-09 00:34:01 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,11 +3,19 @@
 echo "Enable NetworkManager"
 systemctl enable NetworkManager
 
-echo "Install yay"
-
+echo "Create user"
 useradd -m user
 echo "user:user" | chpasswd
 echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+echo "Enable multilib (for Steam)"
+sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+pacman -Sy
+
+echo "Install Steam"
+pacman -S --noconfirm steam
+
+echo "Install yay"
 
 su user <<'EOF'
 
@@ -17,6 +25,14 @@ cd yay
 makepkg -si --noconfirm
 
 yay -S --noconfirm \
+linux-cachyos \
+linux-cachyos-headers \
+appimagelauncher \
+tilix \
+peazip \
+stacer \
+kvantum-qt6 \
+pywal \
 obsidian \
 localsend \
 cursor-bin \
