93bb7f1a6477a7dcd2ee541a445d7e9df559dabe
Jakub668 <jakub.hunek19@gmail.com>
2026-03-09 16:46:51 +0100

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,22 +1,17 @@
 profile_name="arch-jakib"
 iso_label="ARCH_JAKIB"
-iso_publisher="Jakub <github.com/Jakub668>"
-iso_application="Arch Jakib Live/Rescue ISO"
+iso_publisher="Jakub"
+iso_application="Arch Jakib ISO"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-buildmodes=('iso')
+
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito')
+
 arch="x86_64"
 
 pacman_conf="pacman.conf"
 
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd')
-
-file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/customize_airootfs.sh"]="0:0:755"
-)
