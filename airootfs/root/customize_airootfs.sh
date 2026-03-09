0745ff816359bf79780ce7ebf68d9df3f3bbfe46
Jakub668 <jakub.hunek19@gmail.com>
2026-03-09 16:35:33 +0100

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,64 +1,38 @@
 #!/bin/bash
 set -e
 
-echo ">>> Initializing pacman keys"
+echo "Init pacman keys"
 pacman-key --init
 pacman-key --populate archlinux
 
-echo ">>> Enable NetworkManager"
+echo "Enable services"
 systemctl enable NetworkManager
-
-echo ">>> Enable SDDM"
 systemctl enable sddm
+systemctl enable plymouth-start.service
+systemctl enable ananicy-cpp
 
-echo ">>> Enable Plymouth boot splash"
-plymouth-set-default-theme -R arch-jakib
-
-echo ">>> Create live user"
+echo "Create live user"
 useradd -m liveuser
 passwd -d liveuser
-usermod -aG wheel,audio,video,storage liveuser
+usermod -aG wheel,audio,video liveuser
 
-echo ">>> Enable sudo for wheel group"
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-echo ">>> Locale configuration"
+echo "Locale"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-echo ">>> Hostname"
+echo "Hostname"
 echo "arch-jakib" > /etc/hostname
 
-echo ">>> Enable zram"
+echo "Enable Plymouth theme"
+plymouth-set-default-theme -R arch-jakib
+
+echo "Enable ZRAM"
 mkdir -p /etc/systemd/zram-generator.conf.d
 cat <<EOF > /etc/systemd/zram-generator.conf
 [zram0]
 zram-size = ram / 2
 compression-algorithm = zstd
 EOF
-
-echo ">>> Installing yay for AUR packages"
-cd /tmp
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-
-echo ">>> Installing AUR apps"
-sudo -u liveuser yay -S --noconfirm \
-ananicy-cpp \
-gradience \
-flatseal
-
-echo ">>> Enable ananicy service"
-systemctl enable ananicy-cpp
-
-echo ">>> Enable Plymouth service"
-systemctl enable plymouth-start.service
-
-echo ">>> Done!"git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-
-echo "Install AUR packages"
-yay -S --noconfirm ananicy-cpp gradience flatseal
